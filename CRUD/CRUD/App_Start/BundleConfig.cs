a9878a9c4becba8049927a120547e7116feac8b6
André Calza <andre.calza@navegarti.com.br>
2018-01-06 13:27:15 -0200

    ajustes finais e add mascara de moeda

--- CRUD/CRUD/App_Start/BundleConfig.cs
+++ CRUD/CRUD/App_Start/BundleConfig.cs
@@ -27,6 +27,14 @@ namespace CRUD
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/inputmask").Include(
+                      "~/Scripts/inputmask/inputmask.js",
+                      "~/Scripts/inputmask/jquery.inputmask.js",
+                      "~/Scripts/inputmask/inputmask.extensions.js",
+                      "~/Scripts/inputmask/inputmask.date.extensions.js",
+                      "~/Scripts/inputmask/inputmask.numeric.extensions.js"));
+
         }
     }
 }
